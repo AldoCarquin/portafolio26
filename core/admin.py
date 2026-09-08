2719b855c241963b08088fda84698729eb684ef9
Sopa <sopa@MacBook-Pro-de-Sopa.local>
2026-09-08 18:16:16 -0300

    Levantamiento 0 - 080926 1806

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Proyecto, Experiencia, DocumentoCV, MensajeContacto
 
-# Register your models here.
+@admin.register(Proyecto)
+class ProyectoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'orden')
+    ordering = ('orden',)
+
+@admin.register(Experiencia)
+class ExperienciaAdmin(admin.ModelAdmin):
+    list_display = ('puesto', 'empresa', 'fecha_inicio', 'fecha_fin', 'orden')
+    ordering = ('-fecha_inicio', 'orden')
+
+@admin.register(DocumentoCV)
+class DocumentoCVAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'fecha_subida')
+    ordering = ('-fecha_subida',)
+
+@admin.register(MensajeContacto)
+class MensajeContactoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'email', 'asunto', 'fecha_envio')
+    ordering = ('-fecha_envio',)
+    readonly_fields = ('nombre', 'email', 'asunto', 'mensaje', 'fecha_envio')
--- core/views.py
+++ core/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Proyecto, Experiencia, DocumentoCV
+from .forms import ContactoForm
 
-# Create your views here.
+def home(request):
+    proyectos = Proyecto.objects.all()
+    experiencias = Experiencia.objects.all()
+    ultimo_cv = DocumentoCV.objects.first()
+
+    if request.method == 'POST':
+        form = ContactoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, '¡Gracias por tu mensaje! Nos pondremos en contacto contigo pronto.')
+            return redirect('home')
+    else:
+        form = ContactoForm()
+
+    context = {
+        'proyectos': proyectos,
+        'experiencias': experiencias,
+        'ultimo_cv': ultimo_cv,
+        'form': form,
+    }
+
+    return render(request, 'core/index.html', context)
